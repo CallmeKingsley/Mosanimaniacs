2e971321bc21b012afd9cff17a2c40263cac06ba
Dominique <dcummings@mosaiclearning.com>
2019-10-23 16:09:27 -0400

    add styling to question

--- mosanimaniacs/src/components/quiz/Question.js
+++ mosanimaniacs/src/components/quiz/Question.js
@@ -3,12 +3,40 @@ import { connect } from "react-redux";
 import '../../css/Question.css';
 import Countdown from 'react-countdown-now';
 
+const renderer = ({ seconds }) => {
+      return <span>{seconds}</span>;
+  };
+
+    var total = 500;
+    var newNum = 10;
+
+        function pointDecrease(){
+          total = total - newNum;
+        //   setTimeout(pointDecrease, 1000);
+          return total;
+        }
+
+        pointDecrease();
+
 class Question extends Component {
+    
+    constructor(props){
+        super (props);
+        this.state ={
+            total:500
+        }
+    }
+
     render() {
         return (
             <div>
                 <div class="lightGreen">
-                     <div class="darkGrey">This is a test</div>
+                     <div class="darkGrey">
+                         <div class="headerContainer">
+                            <div id="schoolName">South Ohio Technical School</div>
+                            <div id="profileImg"></div>
+                        </div>
+                     </div>
                      <div class="navyBlue"></div>
                      <div class="question-points-section">
                         <div id="answers">
@@ -27,15 +55,20 @@ class Question extends Component {
                         </div>
                         <div id="time">
                             <div id="countdown">
-                                <Countdown date={Date.now() + 30000} zeroPadTime={0} />
+                                 <Countdown
+                                    date={Date.now() + 30000}
+                                    renderer={renderer}/>
                             </div>
                             
                             <div id="seconds"> seconds</div>
+                            <div id="value"> Question Value</div>
+                            <div id="addPoints">{total}</div>
                         </div>
                         <div id="score"></div>
+                        <div></div>
                      </div>
                 </div>
-                <div class="image"></div>
+
             </div>
         )
     }
