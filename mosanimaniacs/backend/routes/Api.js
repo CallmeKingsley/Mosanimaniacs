f7bc5cb07830f3ac661177a2f9f62397b970ff54
kingsley Akpan <kakapan@mosaiclearning.com>
2019-10-23 12:26:19 -0400

    added backend clean up

--- mosanimaniacs/backend/routes/Api.js
+++ mosanimaniacs/backend/routes/Api.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
 const user = require('./user');
 
-router.use('/users',user)
+router.use('/users', user);
 
 
 module.exports = router;
