c2ee88126474017ac32bdbaeb3e5d955346f6885
Shane Harper <sharper@mosaiclearning.com>
2019-11-19 07:37:50 -0500

    started circuit question

--- mosanimaniacs/src/components/admin/questionForms/circuitForm.js
+++ mosanimaniacs/src/components/admin/questionForms/circuitForm.js
@@ -3,17 +3,34 @@ import '../../../css/Welcome.css';
 import '../../../css/createQuiz.css';
 
 class CircuitForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.showCircuitDrawing = this.showCircuitDrawing.bind(this);
+        this.circuitOption = React.createRef();
+        this.circuitDrawing = React.createRef();
+    }
+
+    showCircuitDrawing(e) {
+        e.preventDefaul();
+        e.persist();
+        this.circuitDrawing.style.display = "block";
+    }
+
     render() {
         return (
-            <div>
+            <div className="circuitQuestionForm">
                 <label htmlFor="circuitType">Select Circuit Type:</label>
-                <select id="circuitType">
+                <select id="circuitType" ref={this.circuitOption}>
                     <option value="series" className="circuitOption">
-                        {/* <img srcSet={require("../../../images/Asset3.png")}/> */}
                         Series
                     </option>
                     {/* Add more options here and have image previews */}
                 </select>
+                <div className="circuitImage" ref={this.circuitDrawing}>
+                    {/* This contains a drawing of a simple circuit */}
+                </div>
                 <button className="save">Save</button>
                 <button className="delete">Delete</button>
                 <br/>
